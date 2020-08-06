a772676b3c55f61dbc4a383c74779986a3d25101
mhsl <1114022912@qq.com>
2020-08-06 16:56:13 +0800

    Update and rename install.sh to VuePressInstall.sh

--- VuePressInstall.sh
+++ VuePressInstall.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-echo "My First Script!"
-mkdir test
+echo "VuePress伪一键安装脚本"
+echo "创建packges.json"
+echo '{
+  "name": "miaonie.ga",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "docs:dev": "vuepress dev docs",
+    "docs:build": "vuepress build docs"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}' > package.json
+
 echo '---
 home: true
 heroImage: https://pic.rmb.bdstatic.com/bjh/d85984d541928b368b936a9d74818d4d.png
@@ -15,4 +29,4 @@ features:
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
 footer: MIT Licensed | Copyright © 2018-2020
 --- ' > docs/README.md
-echo "emd"
+echo "创建完毕，请输入 vuepress build docs 构建静态网站"
